3cf93088284f29e88938a6cf0e1764fe254db548
Greg <greg@smartsoftasia.com>
2015-11-25 17:36:11 +0700

    Start add pagination.

---
+++ src/main/java/com/gregb/library/androidhelper/helper/Convertor.java
@@ -0,0 +1,50 @@
+package com.gregb.library.androidhelper.helper;
+
+import java.util.List;
+
+/**
+ * Created by androiddev01 on 11/17/2015 AD.
+ */
+public class Convertor {
+
+  public static int toPrimitive(Integer i){
+    return Validator.isValid(i) ? i : 0;
+  }
+
+  public static boolean toPrimitive(Boolean b){
+    return Validator.isValid(b) ? b : false;
+  }
+  public static long toPrimitive(Long l){
+    return Validator.isValid(l) ? l : 0;
+  }
+
+  public static String notNullAllowEmpty(String s) {
+    return Validator.isValidOrEmpty(s) ? s : "";
+  }
+
+  public static String notNull(String s) {
+    return Validator.isValid(s) ? s : "";
+
+  }
+
+  public static String stringToIntString(String s) {
+    return Validator.isValid(s) ? s : "0";
+  }
+
+  public static int stringToInt(String s) {
+    return Validator.isValid(s) ? Integer.valueOf(s) : 0;
+  }
+
+  public static String toStandartUrl(String url) {
+    if (url != null) {
+      if(url.startsWith("//")){
+        url = "http:" + url;
+      }else if (!url.startsWith("http://") && !url.startsWith("https://")) {
+        url = "http://" + url;
+      }
+    } else {
+      url = "http://";
+    }
+    return url;
+  }
+}
--- src/main/java/com/gregb/library/androidhelper/helper/Validator.java
+++ src/main/java/com/gregb/library/androidhelper/helper/Validator.java
@@ -8,125 +8,85 @@ import java.util.List;
  * Created by gui on 07/08/2014.
  */
 public class Validator {
-    private static final String TAG = "Validator";
+  private static final String TAG = "Validator";
 
-    public static final String SPECIAL_CHARACTERS = "!@#$%^&*()~`-=_+[]{}|:\";',./<>?";
+  public static final String SPECIAL_CHARACTERS = "!@#$%^&*()~`-=_+[]{}|:\";',./<>?";
 
-    public static boolean isValid(String string) {
-        return string != null && !string.trim().equals("") && !string.trim().equals("null");
-    }
+  public static boolean isValid(String s) {
+    return s != null && !s.trim().equals("") && !s.trim().equals("null");
+  }
 
-  public static boolean isValid(List items){
+  public static boolean isValid(Integer i){
+    return i != null;
+  }
+
+  public static boolean isValid(Boolean b){
+    return b != null;
+  }
+
+  public static boolean isValid(Long l){
+    return l != null;
+  }
+
+  public static boolean isValid(List items) {
     return items != null && items.size() > 0;
   }
 
-    public static boolean isValidAllowEmpty(String string) {
-        return string != null && !string.equals("") && !string.equals("null");
-    }
+  public static boolean isValidEmail(String email) {
+    return isValid(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+  }
 
-    public static String validate(String string) {
-        if( string == null || string.trim().equals("") || string.trim().equals("null")){
-            return "";
-        }else{
-            return string;
-        }
-    }
+  public static boolean isValidPassword(String password) {
+    return password.length() > 4;
+  }
 
-    public static String validateAllowBlank(String string) {
-        if( string == null || string.equals("") || string.equals("null")){
-            return "";
-        }else{
-            return string;
-        }
+  public static boolean isValidLenght(String value, int maxValue, int minValue) {
+    if ((maxValue != 0 && value.length() > maxValue) || (minValue != 0
+        && value.length() < minValue)) {
+      return false;
     }
+    return true;
+  }
 
-    public static long validate(Long l) {
-        if( l == null){
-            return 0L;
-        }else{
-            return l;
-        }
-    }
-
-    public static String validateIntString(String string) {
-        if( string == null || string.trim().equals("") || string.trim().equals("null")){
-            return "0";
-        }else{
-            return string;
-        }
-    }
-
-    public static int validateInt(String string) {
-        if( string == null || string.trim().equals("") || string.trim().equals("null")){
-            return 0;
-        }else{
-            return Integer.valueOf(string);
-        }
-    }
+  public static boolean isValidOrEmpty(String string) {
+    return string != null && !string.equals("") && !string.equals("null");
+  }
 
 
-    public static Boolean isValidList(List list){
-        return list != null && list.size() > 0;
-    }
 
-    public static String validateDouble(String string) {
-        if( string == null || string.trim().equals("") || string.trim().equals("null")){
-            return "0.0";
-        }else{
-            return string;
-        }
+  public static String validateUrl(String url) {
+    if (url != null) {
+      if (!url.startsWith("http://") && !url.startsWith("https://")) {
+        url = "http://" + url;
+      }
+    } else {
+      url = "http://";
     }
+    return url;
+  }
 
-    public static String validateUrl(String url){
-        if(url !=null){
-            if (!url.startsWith("http://") && !url.startsWith("https://")){
-                url = "http://" + url;
-            }
-        }else{
-            url = "http://";
-        }
-        return url;
+  /**
+   * Delete + and spaces in the phone number
+   */
+  public static String changePhone(String phone) {
+    if (phone.startsWith("+")) {
+      return phone.substring(1).replaceAll(" ", "");
+    } else {
+      return phone.replaceAll(" ", "");
     }
+  }
 
-    public static boolean isValidEmail(String email, boolean allowBlank) {
-        return !isValid(email) ? allowBlank : android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
-    }
+  public static boolean isSmallScreen(Context c) {
+    return (c.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK)
+        == Configuration.SCREENLAYOUT_SIZE_SMALL;
+  }
 
-    public static boolean isValidPassword(String password){
-        char[] aC = password.toCharArray();
-        for(char c : aC)
-            if (SPECIAL_CHARACTERS.indexOf(String.valueOf(c)) >= 0) return true;
-        return false;
-    }
+  public static boolean isNormalScreen(Context c) {
+    return (c.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK)
+        == Configuration.SCREENLAYOUT_SIZE_NORMAL;
+  }
 
-    public static boolean isValidLenght(String value, int maxValue, int minValue){
-        if((maxValue != 0 && value.length() > maxValue) || (minValue != 0 && value.length() < minValue)) return false;
-        return true;
-    }
-
-    /**
-     * Delete + and spaces in the phone number
-     * @param phone
-     * @return
-     */
-    public static String changePhone(String phone){
-        if(phone.startsWith("+")) return phone.substring(1).replaceAll(" ", "");
-        else return phone.replaceAll(" ", "");
-    }
-
-    public static boolean isSmallScreen(Context c){
-        return (c.getResources().getConfiguration().screenLayout &
-                Configuration.SCREENLAYOUT_SIZE_MASK) ==
-                Configuration.SCREENLAYOUT_SIZE_SMALL;
-    }
-    public static boolean isNormalScreen(Context c){
-        return (c.getResources().getConfiguration().screenLayout &
-                Configuration.SCREENLAYOUT_SIZE_MASK) ==
-                Configuration.SCREENLAYOUT_SIZE_NORMAL;
-    }
-
-    public static boolean isLandscape(Context c){
-        return c.getResources().getConfiguration().orientation ==
-                Configuration.ORIENTATION_LANDSCAPE;
-    }
+  public static boolean isLandscape(Context c) {
+    return c.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
+  }
 }
