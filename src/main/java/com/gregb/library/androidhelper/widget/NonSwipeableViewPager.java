ece0084b8b0eba52b4bece9a3cd87b2a9bb44896
Greg <greg@smartsoftasia.com>
2015-12-22 10:42:31 +0700

    Create Non swipable view page.

---
+++ src/main/java/com/gregb/library/androidhelper/widget/NonSwipeableViewPager.java
@@ -0,0 +1,37 @@
+package com.gregb.library.androidhelper.widget;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+
+/**
+ * Created by androiddev01 on 12/21/2015 AD.
+ */
+public class NonSwipeableViewPager extends ViewPager {
+
+  public NonSwipeableViewPager(Context context) {
+    super(context);
+  }
+
+  public NonSwipeableViewPager(Context context, AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  @Override
+  public boolean onInterceptTouchEvent(MotionEvent event) {
+    // Never allow swiping to switch between pages
+    return false;
+  }
+
+  @Override
+  public boolean onTouchEvent(MotionEvent event) {
+    // Never allow swiping to switch between pages
+    if (event.getAction() == MotionEvent.ACTION_UP) {
+      Log.d("TOUCH", "Up");
+    }
+
+    return false;
+  }
+}
